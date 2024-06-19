877e28bf8e8da74109e18fcb6a57ff50b637daeb
Nikolay Grinchar <navydragon@inbox.ru>
2024-06-19 22:04:43 +0300

    add Task_5_Class_naming.py

---
+++ 1_5_Pure_code/Task_5_Class_naming.py
@@ -0,0 +1,20 @@
+"""
+В рамках данного задания использовались заготовки из самообучния Django REST,  где дедался API для
+'клона' IMDB
+"""
+
+# 1. Улучшите пять имён классов в вашем коде:
+# MovieData - Movie
+# DataProcessor  - DataParser
+# RatingManager  - Rating
+# InfoHandler - Information
+# ReviewManager - Review
+
+# 2. Улучшите семь имён методов и объектов по схеме из пункта 2:
+# fetch_movie - get_movie
+# retrieve_user - get_user
+# fetch_rating - get_rating
+# retrieve_review - get_review
+# fetch_info - get_info
+# create_manager - create_user
+# calculate_rating - handle_rating
