2c6106028a50a1d866293a2d063dd2e45c589700
navydragon <navydragon@inbox.ru>
2024-07-15 12:51:34 +0300

    update tasks 8

--- Recursion/Task_8_Find_all_files.py
+++ Recursion/Task_8_Find_all_files.py
@@ -1,30 +1,28 @@
 import os
 
-
 def find_files(dir_path):
-    if not os.path.isdir(dir_path):
-        return []
-
     all_files = []
-    stack = [dir_path]
 
-    while stack:
-        current_directory = stack.pop()
+    def _find_files_recursive(current_directory):
+        if not os.path.isdir(current_directory):
+            return
 
+        # Получаем список всех файлов
         files_and_dirs = os.listdir(current_directory)
 
+        # Итерация
         for file_or_dir in files_and_dirs:
             full_path = os.path.join(current_directory, file_or_dir)
 
             if os.path.isdir(full_path):
-                stack.append(full_path)
-            elif os.path.isfile(full_path):
+                _find_files_recursive(full_path)
+            else:
                 all_files.append(full_path)
 
+    _find_files_recursive(dir_path)
     return all_files
 
 
-# Пример использования
 directory = 'C:/Users/User/Downloads'
 files = find_files(directory)
 for file in files:
